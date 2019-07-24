65cf3d7163fac9d056c71e53390c19d0bde20be7
KyungMinJin <kyung6479@korea.ac.kr>
2019-07-25 01:33:57 +0900

    commit before yarn eject

---
+++ blog-frontend/src/store/modules/index.js
@@ -0,0 +1,5 @@
+export { default as editor } from "./editor";
+export { default as list } from "./list";
+export { default as base } from "./base";
+export { default as post } from "./post";
+export { penderReducer as pender } from "redux-pender";
