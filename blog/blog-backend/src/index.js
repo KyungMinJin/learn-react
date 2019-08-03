9128ddad16d0b4291ec0ace8f0e48ba551791d5c
KyungMinJin <kyung6479@korea.ac.kr>
2019-08-04 00:23:01 +0900

    문서 폴더로 위치 이동

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,17 +1,13 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 
+const api = require("./api");
+
 const app = new Koa();
 const router = new Router();
 
 //라우터 설정
-router.get("/", ctx => {
-  ctx.body = "홈";
-});
-
-router.get("/about", ctx => {
-  ctx.body = "소개";
-});
+router.use("/api", api.routes()); //api 라우트 적용
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
