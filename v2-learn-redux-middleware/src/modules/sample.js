c59a94e9d3c45187b77eec876e70310a401d6cc2
KyungMinJin <kyung6479@korea.ac.kr>
2019-09-07 01:45:32 +0900

    Saga api 코드 정리 및 select, throttle

--- v2-learn-redux-middleware/src/modules/sample.js
+++ v2-learn-redux-middleware/src/modules/sample.js
@@ -1,6 +1,8 @@
-import { handleActions } from "redux-actions";
+import { createAction, handleActions } from "redux-actions";
+import { call, put, takeLatest } from "redux-saga/effects";
 import * as api from "../lib/api";
-import createRequestThunk from "../lib/createRequestThunk";
+//import createRequestThunk from "../lib/createRequestThunk";
+import { startLoading, finishLoading } from "./loading";
 
 //액션 타입을 선언
 //한 요청 당 세 개를 만들어야 합니다.
@@ -35,8 +37,33 @@ const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 // };
 
 // createRequerstThunk 적용
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+// export const getPost = createRequestThunk(GET_POST, api.getPost);
+// export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+
+export const getPost = createAction(GET_POST, id => id);
+export const getUsers = createAction(GET_USERS);
+
+function* getPostSaga(action) {
+  yield put(startLoading(GET_POST)); // 로딩 시작
+  // 파라미터로 action을 받아 오면 액션의 정보를 조회할 수 있습니다.
+  try {
+    // call 을 사용하면 Promise를 반환하는 함수를 호출하고, 기다릴 수 있습니다.
+    // 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수입니다.
+    const post = yield call(api.getPost, action.payload); // api.getPost(action.payload)를 의미
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post.data
+    });
+  } catch (e) {
+    // try/catch 문을 사용하여 에러도 잡을 수 있습니다.
+    yield put({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true
+    });
+  }
+  yield put(finishLoading(GET_POST)); // 로딩 완료
+}
 
 // export const getUsers = () => async dispatch => {
 //   dispatch({ type: GET_USERS }); // 요청을 시작한 것을 알림
@@ -56,6 +83,28 @@ export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 //   }
 // };
 
+function* getUsersSaga() {
+  yield put(startLoading(GET_USERS));
+  try {
+    const users = yield call(api.getUsers);
+    yield put({
+      type: GET_USERS_SUCCESS,
+      payload: users.data
+    });
+  } catch (e) {
+    yield put({
+      type: GET_USERS_FAILURE,
+      payload: e,
+      error: true
+    });
+  }
+  yield put(finishLoading(GET_USERS));
+}
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 // 초기 상태를 선언
 // 요청의 로딩 중 상태는 loading 이라는 객체에서 관리합니다.
 
