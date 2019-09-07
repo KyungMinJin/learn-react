c5abf5b9cff01e00e7d62e03cef50ecfa3fdb944
KyungMinJin <kyung6479@korea.ac.kr>
2019-09-07 23:21:47 +0900

    7장 라이프사이클 및 에러 발생 페이지

--- v2/hello-react/src/App.js
+++ v2/hello-react/src/App.js
@@ -7,8 +7,23 @@ import EventPracticeF from "./EventPracticeF";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
 import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000"
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
   // const style = {
   //   //background-color는 - 사라지고 카멜 표기법으로 작성됩니다.
   //   backgroundColor: "black",
@@ -17,6 +32,7 @@ class App extends Component {
   //   fontWeight: "bold",
   //   padding: 16 // 단위 생략 시 px
   // };
+
   render() {
     const name = "리액트";
     return (
@@ -38,7 +54,11 @@ class App extends Component {
           맨 밑으로
         </button>
         {/* 5장 */}
-        <IterationSample /> {/* 5장 */}
+        <IterationSample /> {/* 6장 */}
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </>
     );
   }
