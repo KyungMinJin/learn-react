fb3389c8b95b20f522b89c9e400450145626a1c1
KyungMinJin <kyung6479@korea.ac.kr>
2019-09-15 02:13:19 +0900

    11장 New TodoList

---
+++ v2/todo-app/src/components/TodoInsert.js
@@ -0,0 +1,39 @@
+import React, { useState, useCallback } from 'react';
+import { MdAdd } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue('');
+
+      // submit 이벤트는 브라우저에서 새로고침을 발생시킴
+      // 이를 방지하기 위해 이 함수를 호출
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      {/* form 과 onSubmit 으로 처리하면 엔터도 한 번에 처리가능 */}
+      <input
+        placeholder="할 일을 입력하세요."
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
