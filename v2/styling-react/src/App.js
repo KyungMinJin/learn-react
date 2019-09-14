2c5d652c0fecc61054a719562449689ba3784f42
KyungMinJin <kyung6479@korea.ac.kr>
2019-09-14 13:34:46 +0900

    9장 style-react yarn eject 전

---
+++ v2/styling-react/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SassComponent from "./SassComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
